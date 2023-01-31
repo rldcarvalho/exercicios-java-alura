f6ee0affc24af62bfdda783c96b47bec181b6250
rldcarvalho <rldcarvalhoo@gmail.com>
2023-01-31 19:17:29 -0300

    Extraida funcao de reajustar salario

--- arquitetura-design/solid/rh/src/main/java/br/com/alura/rh/model/Funcionario.java
+++ arquitetura-design/solid/rh/src/main/java/br/com/alura/rh/model/Funcionario.java
@@ -21,13 +21,9 @@ public class Funcionario {
 		this.salario = salario;
 	}
 
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
-		this.dataUltimoReajuste = LocalDate.now();
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.salario = novoSalario;
+		dataUltimoReajuste = LocalDate.now();
 	}
 
 	public String getNome() {
