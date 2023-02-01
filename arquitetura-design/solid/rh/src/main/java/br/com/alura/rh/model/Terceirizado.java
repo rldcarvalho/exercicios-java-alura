94884574d94559e64b06f1fb195fe7b14f08b4dc
rldcarvalho <rldcarvalhoo@gmail.com>
2023-02-01 19:02:14 -0300

    Adicionada classe terceirizado

---
+++ arquitetura-design/solid/rh/src/main/java/br/com/alura/rh/model/Terceirizado.java
@@ -0,0 +1,52 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+
+public class Terceirizado {
+
+    private String nome;
+    private String cpf;
+    private Cargo cargo;
+    private BigDecimal salario;
+    private String empresa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+}
