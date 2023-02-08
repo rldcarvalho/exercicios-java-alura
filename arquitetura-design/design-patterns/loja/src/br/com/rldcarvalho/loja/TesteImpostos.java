5f6051ebe8c05f2b9ba6a4abfed706be65aad5cf
rldcarvalho <rldcarvalhoo@gmail.com>
2023-02-08 20:32:12 -0300

    Implementado Decorator

--- arquitetura-design/design-patterns/loja/src/br/com/rldcarvalho/loja/TesteImpostos.java
+++ arquitetura-design/design-patterns/loja/src/br/com/rldcarvalho/loja/TesteImpostos.java
@@ -2,6 +2,7 @@ package br.com.rldcarvalho.loja;
 
 import br.com.rldcarvalho.loja.impostos.CalculadoraDeImpostos;
 import br.com.rldcarvalho.loja.impostos.ICMS;
+import br.com.rldcarvalho.loja.impostos.ISS;
 import br.com.rldcarvalho.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -12,6 +13,6 @@ public class TesteImpostos {
 
         Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, new ICMS()));
+        System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
     }
 }
