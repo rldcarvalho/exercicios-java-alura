abd1b41f9ea252bea861c91983b894d020253954
rldcarvalho <rldcarvalhoo@gmail.com>
2023-02-10 09:48:25 -0300

    Implementado Proxy

--- arquitetura-design/design-patterns/loja/src/br/com/rldcarvalho/loja/TesteComposicao.java
+++ arquitetura-design/design-patterns/loja/src/br/com/rldcarvalho/loja/TesteComposicao.java
@@ -2,6 +2,7 @@ package br.com.rldcarvalho.loja;
 
 import br.com.rldcarvalho.loja.orcamento.ItemOrcamento;
 import br.com.rldcarvalho.loja.orcamento.Orcamento;
+import br.com.rldcarvalho.loja.orcamento.OrcamentoProxy;
 
 import java.math.BigDecimal;
 
@@ -16,7 +17,10 @@ public class TesteComposicao {
         novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
         novo.adicionarItem(antigo);
 
-        System.out.println(novo.getValor());
+        OrcamentoProxy proxy = new OrcamentoProxy(novo);
+
+        System.out.println(proxy.getValor());
+        System.out.println(proxy.getValor());
 
     }
 }
