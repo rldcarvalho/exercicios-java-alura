be620fc43741f91b45cb0ba8571254162ce5b5a8
rldcarvalho <rldcarvalhoo@gmail.com>
2023-02-28 18:32:20 -0300

    Adicionado matricular aluno via use case

---
+++ arquitetura-design/clean-architecture/escola/src/main/java/br/com/rldcarvalho/escola/aplicacao/aluno/matricular/MatricularAlunoDto.java
@@ -0,0 +1,22 @@
+package br.com.rldcarvalho.escola.aplicacao.aluno.matricular;
+
+import br.com.rldcarvalho.escola.dominio.aluno.Aluno;
+import br.com.rldcarvalho.escola.dominio.aluno.CPF;
+import br.com.rldcarvalho.escola.dominio.aluno.Email;
+
+public class MatricularAlunoDto {
+
+    private String nomeAluno;
+    private String cpfAluno;
+    private String emailAluno;
+
+    public MatricularAlunoDto(String nomeAluno, String cpfAluno, String emailAluno) {
+        this.nomeAluno = nomeAluno;
+        this.cpfAluno = cpfAluno;
+        this.emailAluno = emailAluno;
+    }
+
+    public Aluno criarAluno() {
+        return new Aluno(new CPF(cpfAluno), nomeAluno, new Email(emailAluno));
+    }
+}
